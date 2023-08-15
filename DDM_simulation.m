c048acd90d590b66a3fe902923e75f3c13423f07
Jafar Bhatti <111537142+jafar-bhatti@users.noreply.github.com>
2023-08-15 12:33:07 -0400

    first implementation of DDM

--- DDM_simulation.m
+++ DDM_simulation.m
@@ -1,35 +1,64 @@
 % Implementing a DDM
+a = 20;                                         % boundary
+coh = linspace(-.5,.5,10);                      % coherence values (evidence)
+trial_types = length(coh);                      % number of trial types
+num_trials = 1000;                              % number of trials
+RT = zeros(trial_types, num_trials);            % each row is different coherence
+responses = zeros(trial_types, num_trials);     % each column is a different trial 
 
-%VARIABLES
-coh = 1;
-ter = 0.3;  %non-decision time in seconds (s)
-a = 2.0;    %boundary 
-z = 0.5;    %bias
-k = 1;      %slope (drift rate)
 
-%How can the following effect the model?
+% Collect data (1000 trials) for each coherence value 
+for i = 1:trial_types
+    for j = 1:num_trials
+        [dv, cnt, resp] = trial(coh(i), a);
+        RT(i,j) = cnt;
+        responses(i,j) = resp;
+    end
+end
 
-% Reward
-% Inter-trial interval
-% Error timeout
+% Compute reaction time and probabilities from collected data
+meanRT = mean(RT, 2);
+prob_right = sum(responses == 1, 2)/num_trials;
 
-%Evidence
-samples = 10;
-right = randn(samples,1) + coh; %coh could be a sequence, then you just use this line
-left =  randn(samples,1) - coh;
-
-time = 1:samples;
-
+% Plot results 
 figure
-hold on;
-subplot(1,2,1);
-plot(time, right)
-plot(time, left)
-title('Evidence')
-hold off;
-subplot(1,2,2);
-diffusion = cumsum(right);
-plot(time, diffusion)
-title('Decision Variable');
+subplot(3,1,1)
+plot(1:length(dv), cumsum(dv));
+yline(a);
+yline(a.*-1)
+ylim([-1.5.*a,1.5.*a])
+ylabel('Arbitrary units')
+xlabel('Time (samples)')
+title('Decision Variable (single-trial example)')
+subplot(3,1,2)
+plot(coh, meanRT,'-ko');
+xlabel('Signed Coherence')
+ylabel('RT in samples')
+subplot(3,1,3)
+plot(coh, prob_right,'-ko');
+xlabel('Signed Coherence')
+ylabel('Probability Right')
+set(gcf,'position',[700,200,300,500])
+
+%ACCUMULATE EVIDENCE OVER TIME UNTIL BOUNDARY CROSSED
+function [dv, cnt, resp] = trial(coh, a)
+k = 0.3;
+dv = [];
+cnt = 1;
+resp = [];
+
+while cnt < 10000
+    if max(cumsum(dv)) >= a
+        resp = 1;
+        break
+    elseif min(cumsum(dv)) <= a*-1
+        resp = -1;
+        break
+    else 
+        dv(cnt,1) = randn + (coh.*k);
+        cnt = cnt + 1;
+    end
+end
+end
 
 
